3ca8b63c3c2f10c8c6ced58e0cf6cdce208239bf
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-25 06:24:16 +0700

    Update help.js

--- language/help.js
+++ language/help.js
@@ -4,7 +4,7 @@ Saya ${botname}, Bot Ini Adalah Beta Multi-Device WhatsApp.
 Jika Ada Fitur Error Atau Bug Segera Lapor Ke Owner Bot
 
   *INFO BOT*
-  👑 Creator : *Lexxy Official*
+  👑 Creator : *Pratama Official*
   👤 Owner : *${ownername}*
   🤖 Bot Name : *${botname}*
   📑 Tanggal : *${tanggal}*
